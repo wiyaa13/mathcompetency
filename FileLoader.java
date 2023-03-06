21a66decf4c2edf1a480966747d5a708f5e081f5
wiyaa13 <126445322+wiyaa13@users.noreply.github.com>
2023-03-06 23:16:07 +0800

    Add files via upload

---
+++ FileLoader.java
@@ -0,0 +1,21 @@
+public class FileLoader {
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    private String filename;
+
+    public FileLoader(String filename) {
+        this.filename = filename;
+    }
+
+    public static void main(String args[]) {
+
+
+
+    }
+}
